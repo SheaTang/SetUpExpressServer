b8f799392169d8a6e773539182a47040b8bbc76c
Sheena Bish Tang <101611557+SheaTang@users.noreply.github.com>
2023-05-22 22:12:45 -0500

    Set Up Express Server

---
+++ simple-server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+//base URL response
+app.get("/", function (req, res) {
+  res.send(`On Express server running on port ${port}...`);
+});
+
+// data route
+app.get("/data", function (req, res) {
+  res.send("data will show here...");
+});
+
+// initial port
+app.listen(port, function () {
+  console.log(`Running on port ${port}...`);
+});
